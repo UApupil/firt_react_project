2cae01dfc37c6d0a51d09ef3d7eca335481ce34a
Valerii Kozurak <vkozak09@gmail.com>
2021-02-25 22:13:15 +0200

    Improved TodoItem, removeTodo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
     const [todos, setTodos] = React.useState([
@@ -19,12 +20,18 @@ function App() {
         )
     }
 
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id != id))
+    }
+
     return (
+        <Context.Provider value={{ removeTodo }}>
         <div className="wrapper">
             <h1>My Todo List</h1>
 
-            <TodoList todos={todos} onToggle={toggleTodo}/>
+            <TodoList todos={todos} onToggle={toggleTodo} />
         </div>
+        </Context.Provider>
     );
 }
 
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from 'prop-types';
+import Context from "../context";
 
 const styles = {
     li: {
@@ -17,11 +18,19 @@ const styles = {
 }
 
 function TodoItem({ todo, index, onChange }) {
-    console.log('todo', todo)
+    const { removeTodo } = useContext(Context)
+    const classes = []
+
+    if (todo.completed) {
+        classes.push('done')
+    }
+
     return (
       <li style={styles.li}>
-         <span>
-             <input type="checkbox"
+         <span className={classes.join(' ')}>
+             <input
+                    type="checkbox"
+                    checked={todo.completed}
                     style={styles.input}
                     onChange={() => onChange(todo.id)}
              />
@@ -30,7 +39,9 @@ function TodoItem({ todo, index, onChange }) {
              {todo.title}
          </span>
 
-         <button className='rm'>&times;</button>
+         <button className='rm' onClick={removeTodo.bind(null, todo.id)}>
+             &times;
+         </button>
       </li>
     )
 }
