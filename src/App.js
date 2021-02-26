c9163c6049fb98abb6f685e533dc2781f9a62b74
Valerii Kozurak <vkozak09@gmail.com>
2021-02-27 00:00:43 +0200

    Added setTodos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
     const [todos, setTodos] = React.useState([
@@ -21,16 +22,30 @@ function App() {
     }
 
     function removeTodo(id) {
-        setTodos(todos.filter(todo => todo.id != id))
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    function addTodo(title) {
+        setTodos(todos.concat([
+            {
+                title,
+                id: Date.now(),
+                completed: false
+            }
+        ]))
     }
 
     return (
-        <Context.Provider value={{ removeTodo }}>
-        <div className="wrapper">
-            <h1>My Todo List</h1>
-
-            <TodoList todos={todos} onToggle={toggleTodo} />
-        </div>
+        <Context.Provider value={{removeTodo}}>
+            <div className="wrapper">
+                <h1>My Todo List</h1>
+                <AddTodo onCreate={addTodo} />
+                {todos.length ? (
+                    <TodoList todos={todos} onToggle={toggleTodo}/>
+                ) : (
+                    <p>No todos</p>
+                )}
+            </div>
         </Context.Provider>
     );
 }
